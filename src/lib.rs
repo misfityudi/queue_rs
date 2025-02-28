b5ff462b6878dd7691d4f92893202a23ebb81a48
Vikas Chahar <misfityudi@gmail.com>
2025-02-28 12:54:13 +0530

    add enqueue method and tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ impl<T: Clone> Queue<T> {
     pub fn new() -> Self {
         Self { items: Vec::new() }
     }
+
+    pub fn enqueue(&mut self, item: T) {
+        self.items.push(item);
+    }
 }
 
 #[cfg(test)]
@@ -16,5 +20,13 @@ mod tests {
     fn new() {
         let queue = Queue::<i8>::new();
         assert!(queue.items.is_empty());
+    }
+
+    #[test]
+    fn enqueue() {
+        let mut queue = Queue::new();
+        let item = i8::MAX;
+        queue.enqueue(item);
+        assert_eq!(queue.items.len(), 1);
     }
 }
